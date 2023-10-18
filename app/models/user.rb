4516422c5a6f0e338b2962486dda6c8a552a45e2
dwddeer <jeleniogrod@gmail.com>
2023-10-19 01:39:35 +0200

    workinggit add -A

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,12 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    #unless user
-    #  user = User.create(
-    #   email: data['email'],
-    #   password: Devise.friendly_token[0,20]
-    #  )
-    #end
+    unless user
+      user = User.create(
+       email: data['email'],
+       password: Devise.friendly_token[0,20]
+      )
+    end
     user
   end
 
