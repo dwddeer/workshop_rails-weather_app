3377b31817df263cb8c33943dd811a053f520611
DJ <jeleniogrod@gmail.com>
2023-10-23 19:14:43 +0200

    sidekiq working (pokazuje godzine w tył)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,5 +6,10 @@ class UserMailer < ApplicationMailer
         mail(to: book_loan.user.email, subject: 'Siema')
     end
 
+    def due_date_notification_email
+        @book_title = book_loan.book.title
+        @due_date = book_loan.due_date
 
+        mail(to: book_loan.user.email, subject: 'Siema')
+    end
 end 
