d31bb47e536bd4c20125f9bfadff917cfc57ed2a
DJ <jeleniogrod@gmail.com>
2023-10-30 20:32:28 +0100

    test working

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Log in', type: :feature do
+  let(:user) { create(:user) }
+
+  before do
+    visit new_user_session_path
+  end
+
+  context 'when user is not registered' do
+    let(:email) { 'not_existing@email.com' }
+    let(:password) { 'password' }
+
+    it 'displays error message' do # wszystkie capybarowe akcje wykonujemy w bloku it
+      within '#new_user' do
+        fill_in 'user_email',	with: email
+        fill_in 'user_password',	with: password
+        click_button 'Log in'
+      end
+
+      expect(page).to have_content('Invalid Email or password.')
+    end
+  end
+end
